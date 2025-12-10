a29a40afdbacb7de938e35bfc5d5b278b148f4e8
Aizel Joy Lopez <aizeljoylopezz@gmail.com>
2025-12-10 17:18:37 +0800

    Updated console log

--- tindahan-frontend/src/components/ProductList.jsx
+++ tindahan-frontend/src/components/ProductList.jsx
@@ -9,7 +9,8 @@ const API_BASE =
   DEFAULT_API;
 
 const API_URL = `${API_BASE.replace(/\/+$/, "")}/api/products`;
-// Debug logs (remove after verifying)
+
+
 console.log("API_BASE", API_BASE);
 console.log("API_URL", API_URL);
 
