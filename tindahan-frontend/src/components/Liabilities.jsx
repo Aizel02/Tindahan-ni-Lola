ad335894f094e02793de91472adfd6e019a01da3
Aizel Joy Lopez <aizeljoylopezz@gmail.com>
2026-01-17 16:40:16 +0800

    removing navlink in productlist

--- tindahan-frontend/src/components/Liabilities.jsx
+++ tindahan-frontend/src/components/Liabilities.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import "./Liabilities.css";
+import { NavLink } from "react-router-dom";
 
 export default function Liabilities() {
   return (
